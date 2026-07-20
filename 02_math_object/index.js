924788e099bb3e0d4e55b3d8e6e5d8ee5f5716ed
Khairuliman-ai <khairuliman736@gmail.com>
2026-07-20 16:55:57 +0800

    math object

---
+++ 02_math_object/index.js
@@ -0,0 +1,28 @@
+// Math = built-in object that provides a
+//  collection of properties and method
+
+console.log(Math.PI);
+console.log(Math.E);
+let x = 3.21;
+let y = 2;
+let z1 = Math.round(x);
+let z2 = Math.floor(x);
+let z3 = Math.ceil(x);
+let z4 = Math.trunc(x);
+let z5 = Math.pow(y,x);
+let z6 = Math.sqrt(x);
+let z7 = Math.abs(x);
+let z8 = Math.max(x,y);
+let z9 = Math.min(x,y);
+let z10 = Math.random();
+
+console.log(z1);
+console.log(z2);
+console.log(z3);
+console.log(z4);
+console.log(z5);
+console.log(z6);
+console.log(z7);
+console.log(z8);
+console.log(z9);
+console.log(z10);
