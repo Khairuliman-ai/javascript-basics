12543ed4786b9e1f8db5a1c18f3cc70d8d8ad928
Khairuliman-ai <khairuliman736@gmail.com>
2026-07-20 21:21:05 +0800

    ternary operation

---
+++ 06_ternary_operator/index.js
@@ -0,0 +1,7 @@
+// ternary operator =  condition ? exprIfTrue : exprIfFalse;
+// it is shortcut for if/else
+
+
+let age = 22;
+let message = age >= 18 ? "You are adult." : "You are minor.";
+console.log(message);
