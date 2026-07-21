4db37bab2b2284b95f67f754dbdc50311fa0e911
Khairuliman-ai <khairuliman736@gmail.com>
2026-07-21 22:14:58 +0800

    for loop

---
+++ 11_for_loop/index.js
@@ -0,0 +1,14 @@
+// for loop = repeats a specific portion of code when a certain condition appears to be true
+
+//  start          end         step
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+
+let total = 0;
+for (let i = 0; i <= 10; i++) {
+    total += i;
+    console.log(total);
+}
+console.log(total);
