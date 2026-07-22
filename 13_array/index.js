aea3b91b779b8c8201bcad7616deb88db1d199fb
Khairuliman-ai <khairuliman736@gmail.com>
2026-07-22 10:52:25 +0800

    array

---
+++ 13_array/index.js
@@ -0,0 +1,28 @@
+// array = a variable like a structure that can holde more than  1 value in a single name
+
+let fruits = ["Apple", "banana", "orange", "mango"];
+
+/*
+fruits.push("Papaya"); // adding 
+fruits.pop(); // removing
+fruits.unshift("Pineapple"); // adding at front
+fruits.shift(); // removing at front
+fruits.short().reverse(); // reversing
+fruits.sort(); // sorting
+fruits.slice(0,2); // slicing
+
+
+// array indexing
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[3]);
+console.log(fruits[4]); 
+
+let numOfFruits = fruits.indexOf("banana");
+console.log(numOfFruits); //indexOf tells the index of the element
+*/
+
+for(let i = 0; i <= fruits.length; i++){
+    console.log(fruits[i]);
+}
